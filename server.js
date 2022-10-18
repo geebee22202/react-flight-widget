25b13294de53abbd9c4ba2e8297fbb4163d289ce
geebee22202 <90569659+geebee22202@users.noreply.github.com>
2022-10-17 23:51:40 -0400

    grabbed data from backend into TableBody

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const { response } = require("express");
 require("dotenv").config();
 
 const app = express();
+app.use(cors())
+
 app.get("/flights", (req, res) => {
   const options = {
     url: `${process.env.URL}?page-size=6`,
--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -1,5 +1,21 @@
+import { useState, useEffect } from 'react'
+
 const TableBody = () => {
-  return <></>;
+  const [flights, setFlights] = useState(null)
+  const getFlights = () => {
+    fetch("http://localhost:8000/flights")
+      .then(response => response.json())
+      .then(flights => setFlights(flights))
+      .catch(err => console.log(err));
+  }
+
+  useEffect(() => getFlights(), [])
+  console.log(flights)
+  return (
+    <tbody>
+
+    </tbody>
+  );
 };
 
 export default TableBody;
